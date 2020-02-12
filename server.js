e20f071e7913f8e1bb4ee297c3ffd670e23ed6f3
Jasmine Terry <jasmine.terry91@gmail.com>
2020-02-12 15:02:25 -0500

    dependencies download. server started

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
-
+const hemlet = require('helmet')
 const server = express();
+const userRouter =require('./users/userRouter');
+const postRouter =require('./posts/postRouter');
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+//middleware
+server.use(express.json());
+server.use(hemlet());
+
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+
+}
+
+
+//routes
+server.get('/', (req, res) => {
+  console.log('server start res')
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+
+
 
 module.exports = server;
